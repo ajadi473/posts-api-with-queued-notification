bec8f3310644873ba46a68a9d9c5fdd3c5799a4f
ajadi.paul <paul.ajadi@chidalusoftware.com>
2022-03-21 18:36:30 +0100

    Feature: automatic delete

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Models\posts;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +18,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function() {
+            posts::where('created_at', '<', Carbon::now()->subDays(15))->delete();
+        })->daily();
     }
 
     /**
